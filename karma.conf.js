10ac726a6f8275e5f7c5d91011fa5274e2a53ea4
ViktorSoroka <montanaqw5809@gmail.com>
2016-07-30 20:46:48 +0300

    remove test coverasge

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,14 @@
-module.exports = function (config) {
+module.exports = function(config) {
     config.set({
         frameworks: ['browserify', 'jasmine'],
-        reporters: ['progress', 'coverage'],
         files: [
             'src/**/*.js',
             'test/**/*_spec.js'
         ],
-
         preprocessors: {
             'test/**/*.js': ['jshint', 'browserify'],
-            'src/**/*.js': ['jshint', 'browserify', 'coverage']
+            'src/**/*.js': ['jshint', 'browserify']
         },
-
         browsers: ['Chrome'],
 
         autoWatch: true,
@@ -19,11 +16,6 @@ module.exports = function (config) {
         browserify: {
             debug: true,
             bundleDelay: 2000 // Fixes "reload" error messages, YMMV!
-        },
-
-        coverageReporter: {
-            type: 'lcov',
-            dir: 'coverage'
         }
     })
 };
