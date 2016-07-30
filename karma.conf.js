24acd56dba95e64fe9e79df84a528ba8b8521707
ViktorSoroka <montanaqw5809@gmail.com>
2016-07-30 19:26:50 +0300

    add test coverasge

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,17 @@
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
         frameworks: ['browserify', 'jasmine'],
+        reporters: ['progress', 'coverage'],
         files: [
             'src/**/*.js',
             'test/**/*_spec.js'
         ],
+
         preprocessors: {
             'test/**/*.js': ['jshint', 'browserify'],
-            'src/**/*.js': ['jshint', 'browserify']
+            'src/**/*.js': ['jshint', 'browserify', 'coverage']
         },
+
         browsers: ['Chrome'],
 
         autoWatch: true,
@@ -16,6 +19,11 @@ module.exports = function(config) {
         browserify: {
             debug: true,
             bundleDelay: 2000 // Fixes "reload" error messages, YMMV!
+        },
+
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage'
         }
     })
 };
