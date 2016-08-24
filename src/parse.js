d6fca6c3e6904ddaddc7d0cd6a59dc776cc9e0b4
ViktorSoroka <montanaqw5809@gmail.com>
2016-08-24 11:52:46 +0300

    finish parsing primitives

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var ESCAPES = {
+    'n' : '\n',
+    'f' : '\f',
+    'r' : '\r',
+    't' : '\t',
+    'v' : '\v',
+    '\'': '\'',
+    '"' : '"'
+};
+
 function parse(expr) {
     var lexer  = new Lexer();
     var parser = new Parser(lexer);
@@ -10,15 +20,21 @@ function Lexer() {
 }
 
 Lexer.prototype.lex = function (text) {
-    this.text   = text;
-    this.index  = 0; // Our current character index in the string
-    this.ch     = undefined; // The current character
+    this.text  = text;
+    this.index = 0; // Our current character index in the string
+    this.ch = undefined; // The current character
     this.tokens = []; // The resulting collection of tokens.
 
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
         if (this.isNumber(this.ch) || (this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
+        } else if (this.ch === '\'' || this.ch === '"') {
+            this.readString(this.ch);
+        } else if (this.isIdent(this.ch)) {
+            this.readIdent();
+        } else if (this.isWhitespace(this.ch)) {
+            this.index++;
         } else {
             throw 'Unexpected next character: ' + this.ch;
         }
@@ -31,8 +47,18 @@ Lexer.prototype.isNumber = function (ch) {
     return typeof ch !== 'boolean' && '0' <= ch && ch <= '9';
 };
 
-Lexer.prototype.isExpOperator = function(ch) {
+Lexer.prototype.isExpOperator = function (ch) {
     return ch === '-' || ch === '+' || this.isNumber(ch);
+};
+
+Lexer.prototype.isIdent = function (ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+        ch === '_' || ch === '$';
+};
+
+Lexer.prototype.isWhitespace = function(ch) {
+    return ch === ' ' || ch === '\r' || ch === '\t' ||
+        ch === '\n' || ch === '\v' || ch === '\u00A0';
 };
 
 Lexer.prototype.peek = function () {
@@ -57,7 +83,7 @@ Lexer.prototype.readNumber = function () {
                 nextCh && this.isNumber(nextCh)) {
                 number += ch;
             } else if (this.isExpOperator(ch) && prevCh === 'e' &&
-                (!nextCh || !this.isNumber(nextCh))) {
+                (!nextCh || !this.isNumber(nextCh)) || ch === 'e' && !this.isExpOperator(nextCh)) {
                 throw 'Invalid exponent';
             } else {
                 break;
@@ -72,10 +98,69 @@ Lexer.prototype.readNumber = function () {
     });
 };
 
+Lexer.prototype.readString = function (quote) {
+    this.index++;
+    var string = '';
+    var escape = false;
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (escape) {
+            if (ch === 'u') {
+                var hex = this.text.substring(this.index + 1, this.index + 5);
+                if (!hex.match(/[\da-f]{4}/i)) {
+                    throw 'Invalid unicode escape';
+                }
+                this.index += 4;
+                string += String.fromCharCode(parseInt(hex, 16));
+            } else {
+                var replacement = ESCAPES[ch];
+                if (replacement) {
+                    string += replacement;
+                } else {
+                    string += ch;
+                }
+            }
+            escape = false;
+        } else if (ch === quote) {
+            this.index++;
+            this.tokens.push({
+                text : string,
+                value: string
+            });
+            return;
+        } else if (ch === '\\') {
+            escape = true;
+        } else {
+            string += ch;
+        }
+        this.index++;
+    }
+    throw 'Unmatched quote';
+};
+
+Lexer.prototype.readIdent = function () {
+    var text = '';
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (this.isIdent(ch) || this.isNumber(ch)) {
+            text += ch;
+        } else {
+            break;
+        }
+        this.index++;
+    }
+    var token = { text: text };
+
+    this.tokens.push(token);
+};
+
 
 function AST(lexer) {
     this.lexer = lexer;
 }
+
+AST.Program = 'Program';
+AST.Literal = 'Literal';
 
 AST.prototype.ast = function (text) {
     this.tokens = this.lexer.lex(text);
@@ -83,20 +168,36 @@ AST.prototype.ast = function (text) {
 };
 
 AST.prototype.program = function () {
-    return { type: AST.Program, body: this.constant() };
+    return { type: AST.Program, body: this.primary() };
+};
+
+AST.prototype.primary = function() {
+    if (this.constants.hasOwnProperty(this.tokens[0].text)) {
+        return this.constants[this.tokens[0].text];
+    } else {
+        return this.constant();
+    }
 };
 
 AST.prototype.constant = function () {
     return { type: AST.Literal, value: this.tokens[0].value };
 };
 
-AST.Program = 'Program';
-AST.Literal = 'Literal';
-
+AST.prototype.constants = {
+    'null' : { type: AST.Literal, value: null },
+    'true' : { type: AST.Literal, value: true },
+    'false': { type: AST.Literal, value: false }
+};
 
 function ASTCompiler(astBuilder) {
     this.astBuilder = astBuilder;
 }
+
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+
+ASTCompiler.prototype.stringEscapeFn = function (c) {
+    return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
+};
 
 ASTCompiler.prototype.compile = function (text) {
     var ast    = this.astBuilder.ast(text);
@@ -113,10 +214,21 @@ ASTCompiler.prototype.recurse = function (ast) {
             this.state.body.push('return ', this.recurse(ast.body), ';');
             break;
         case AST.Literal:
-            return ast.value;
+            return this.escape(ast.value);
     }
 };
 
+ASTCompiler.prototype.escape = function (value) {
+    if (_.isString(value)) {
+        return '\'' +
+            value.replace(this.stringEscapeRegex, this.stringEscapeFn) +
+            '\'';
+    } else if (_.isNull(value)) {
+        return 'null';
+    } else {
+        return value;
+    }
+};
 
 function Parser(lexer) {
     this.lexer       = lexer;
